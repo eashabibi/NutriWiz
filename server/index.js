e3c0c9c7e176c7a30f2fdf3563562480426e6b46
Epanagandla Asha Bibi <e.ashabibi123@gmail.com>
2023-09-23 12:38:22 +0530

    changed server index page

--- server/index.js
+++ server/index.js
@@ -72,7 +72,10 @@ app.post("/login", async (req, res) => {
   }
 });
 
+console.log("Asha");
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
